6d9b9c58d75ab1a49643b748bf28685f61155364
sean <rzha0066@student.monash.edu>
2021-05-15 21:39:17 +0800

    topnav样式更新

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import { createWebHashHistory, createRouter } from 'vue-router'
+
 const history = createWebHashHistory()
 const router = createRouter({
   history,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="log"></div>
-      <div class="menu"></div>
+      <div class="logo"></div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>Efficient UI</h1>
@@ -20,7 +23,21 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.topnar {
-  border: 1px solid red;
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
 }
 </style>
