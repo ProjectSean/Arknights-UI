0fbb342983ce571c09eb910cba5a9c41b84cf8de
sean <rzha0066@student.monash.edu>
2021-05-15 15:29:44 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Sean />
+</template>
+
+<script>
+import Sean from "./components/Sean.vue";
+
+export default {
+  name: "App",
+  components: {
+    Sean,
+  },
+};
+</script>
---
+++ src/components/Sean.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>我的第一个组件</div>
+</template>
+
+<script>
+export default {
+  name: "Sean",
+};
+</script>
+
+<style scoped>
+</style>
