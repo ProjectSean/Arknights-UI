a8a74ff33c79088059a2177e67724d06e45da7cc
sean <seanrz0066@gmail.com>
2021-05-16 11:00:27 +0800

    hover之后右滑动

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,18 @@
+<template>
+  <Switch />
+</template>
+
+<script lang="ts">
+import Switch from "../lib/Switch.vue";
+export default {
+  setup() {
+    return {};
+  },
+  components: {
+    Switch,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/network/router.ts
+++ src/network/router.ts
@@ -2,10 +2,10 @@ import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from '../views/Home.vue'
 import Doc from '../views/Doc.vue'
 import Docdemo from '../components/Docdemo.vue'
-import Switch from '../components/Switch.vue'
-import Dialog from '../components/Dialog.vue'
-import Tabs from '../components/Tabs.vue'
-import Button from '../components/Button.vue'
+import Switch from '../components/SwitchDemo.vue'
+import Dialog from '../components/DialogDemo.vue'
+import Tabs from '../components/TabsDemo.vue'
+import Button from '../components/ButtonDemo.vue'
 
 const history = createWebHashHistory()
 export const router = createRouter({
