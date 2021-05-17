f18a404db2587f071e3042227c0a0371656fb473
sean <seanrz0066@gmail.com>
2021-05-17 23:15:24 +0800

    disabled属性添加

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,9 +3,9 @@
   <h1>示例1</h1>
   <div>
     <Button>确认</Button>
-    <Button theme="button">确认</Button>
     <Button theme="link">跳转</Button>
     <Button theme="text">文字按钮</Button>
+    <Button theme="danger">确认</Button>
   </div>
   <h1>示例2</h1>
   <div>
@@ -24,30 +24,24 @@
       <Button theme="text">普通按钮</Button>
       <Button theme="text" size="small">小按钮</Button>
     </div>
+    <div>
+      <Button theme="danger" size="big">大按钮</Button>
+      <Button theme="danger">普通按钮</Button>
+      <Button theme="danger" size="small">小按钮</Button>
+    </div>
   </div>
   <h1>示例3</h1>
   <div>
-    <div>
-      <Button level="main">主要按钮</Button>
-      <Button>普通按钮</Button>
-      <Button level="danger">危险按钮</Button> <br />
-    </div>
-    <div>
-      <Button theme="link" level="main">主要按钮</Button>
-      <Button theme="link">普通按钮</Button>
-      <Button theme="link" level="danger">危险按钮</Button> <br />
-    </div>
-    <div>
-      <Button theme="text" level="main">主要按钮</Button>
-      <Button theme="text">普通按钮</Button>
-      <Button theme="text" level="danger">危险按钮</Button> <br />
-    </div>
+    <Button disabled>主要按钮</Button>
+    <Button theme="link" disabled>未开放</Button>
+    <Button theme="text" disabled>NO INFO</Button>
+    <Button theme="danger" disabled>普通按钮</Button>
   </div>
   <h1>示例4</h1>
   <div>
-    <Button loading>加载中</Button>
-    <Button theme="link" size="big" loading>加载中</Button>
-    <Button theme="text" size="small" loading>加载中</Button>
+    <Button theme="link" size="big" loading>等待中</Button>
+    <Button theme="text" loading>加载中</Button>
+    <Button size="small" loading>加载中</Button>
   </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -6,9 +6,27 @@
     @mousedown="onMouseDown"
     @mouseup="onMouseUp"
   >
-    <span v-if="loading" class="ark-loadingIndicator"></span>
+    <div v-if="loading && size === 'big'" class="ark-loader">
+      <span class="ark-loader-block"></span>
+      <span class="ark-loader-block"></span>
+      <span class="ark-loader-block"></span>
+      <span class="ark-loader-block"></span>
+      <span class="ark-loader-block"></span>
+      <span class="ark-loader-block"></span>
+      <span class="ark-loader-block"></span>
+      <span class="ark-loader-block"></span>
+      <span class="ark-loader-block"></span>
+    </div>
+    <div v-else-if="loading && size === 'small'" class="ark-loader"></div>
+    <div v-else-if="loading" class="ark-loader">
+      <div class="ark-cube-spin ark-cube-spin1"></div>
+      <div class="ark-cube-spin ark-cube-spin2"></div>
+      <div class="ark-cube-spin ark-cube-spin4"></div>
+      <div class="ark-cube-spin ark-cube-spin3"></div>
+    </div>
     <div><slot /></div>
-    <span :class="{ 'ark-span-arrow': theme === 'link' }"></span>
+    <span v-if="disabled" class="ark-span-diabled"></span>
+    <span :class="{ 'ark-span-arrow': theme === 'link' && !loading }"></span>
   </button>
 </template>
 
@@ -78,6 +96,7 @@ $link-bg-active: #2a2a2a;
 $link-text-active: #c4c4c4;
 $radius: 4px;
 button.ark-button {
+  position: relative;
   box-sizing: border-box;
   height: $h;
   padding: 0 24px;
@@ -92,6 +111,7 @@ button.ark-button {
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(#000, 0.95);
   font-weight: 700;
+  font-size: 16px;
   & + & {
     margin-left: 16px;
     margin-bottom: 16px;
@@ -104,17 +124,130 @@ button.ark-button {
   }
   &.ark-size-big {
     min-width: 136px;
-    > .ark-loadingIndicator {
-      width: 20px;
-      height: 20px;
-      border-radius: 12px;
+    font-size: 22px;
+    height: $h + 16px;
+    padding: 0 32px;
+    > .ark-loader {
+      position: absolute;
+      top: 50%;
+      left: 6%;
+      width: 21px;
+      height: 21px;
+      transform: translate(0, -40%);
+      > .ark-loader-block {
+        display: inline-block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 6px;
+        height: 6px;
+        opacity: 0;
+        background: #fdfdfd;
+        animation: show 880ms step-end infinite alternate,
+          pulse 880ms linear infinite alternate;
+        &:nth-child(1) {
+          transform: translate(0, 0);
+          animation-delay: 65ms;
+        }
+        &:nth-child(2) {
+          transform: translate(7.5px, 0);
+          animation-delay: 130ms;
+        }
+        &:nth-child(3) {
+          transform: translate(15px, 0);
+          animation-delay: 195ms;
+        }
+        &:nth-child(4) {
+          transform: translate(0, 7.5px);
+          animation-delay: 325ms;
+        }
+        &:nth-child(5) {
+          transform: translate(7.5px, 7.5px);
+          animation-delay: 130ms;
+        }
+        &:nth-child(6) {
+          transform: translate(15px, 7.5px);
+          animation-delay: 455ms;
+        }
+        &:nth-child(7) {
+          transform: translate(0, 15px);
+          animation-delay: 390ms;
+        }
+        &:nth-child(8) {
+          transform: translate(7.5px, 15px);
+          animation-delay: 260ms;
+        }
+        &:nth-child(9) {
+          transform: translate(15px, 15px);
+        }
+      }
     }
   }
   &.ark-size-small {
-    > .ark-loadingIndicator {
+    font-size: 8px;
+    height: $h - 8px;
+    padding: 0 16px;
+    > .ark-loader {
+      position: absolute;
+      left: 0;
+      margin-left: 4px;
       width: 8px;
       height: 8px;
-      border-radius: 6px;
+      display: inline-block;
+      margin-right: 4px;
+      border-radius: 4px;
+      border-color: #fff #fff #fff transparent;
+      border-style: solid;
+      border-width: 2px;
+      animation: ark-circle-spin 1s infinite linear;
+    }
+  }
+  &.ark-size-normal {
+    > .ark-loader {
+      position: absolute;
+      display: inline-block;
+      height: 12px;
+      width: 12px;
+      right: 0;
+      left: 0;
+      margin-left: 6px;
+      transform: rotateZ(45deg);
+      > .ark-cube-spin {
+        float: left;
+        width: 50%;
+        height: 50%;
+        position: relative;
+        transform: scale(1.1);
+        &::before {
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: #fff;
+          transform-origin: 100% 100%;
+          animation: ark-cube-angle 2.4s infinite linear both;
+        }
+        &.ark-cube-spin2 {
+          transform: scale(1.1) rotateZ(90deg);
+          &::before {
+            animation-delay: 300ms;
+          }
+        }
+        &.ark-cube-spin3 {
+          transform: scale(1.1) rotateZ(180deg);
+          &::before {
+            animation-delay: 600ms;
+          }
+        }
+        &.ark-cube-spin4 {
+          transform: scale(1.1) rotateZ(270deg);
+          &::before {
+            animation-delay: 900ms;
+          }
+        }
+      }
     }
   }
   &.ark-theme-button {
@@ -123,15 +256,16 @@ button.ark-button {
       background-image: none;
       background-color: $button-bg-active;
     }
-    &.ark-size-big {
-      font-size: 24px;
-      height: $h + 16px;
-      padding: 0 32px;
+    &[disabled] {
+      cursor: not-allowed;
+      color: #fff;
     }
-    &.ark-size-small {
-      font-size: 8px;
-      height: $h - 8px;
-      padding: 0 16px;
+    > .ark-span-diabled {
+      position: absolute;
+      width: calc(100% + 2px);
+      height: calc(100% + 2px);
+      border-radius: $radius;
+      background-color: rgba(199, 199, 199, 0.7);
     }
   }
   &.ark-theme-link {
@@ -148,6 +282,14 @@ button.ark-button {
       border-bottom: 4px solid transparent;
       border-left: 5px solid $link-span;
     }
+    &[disabled] {
+      border: 1px solid transparent;
+      background-color: #c7c7c7;
+      cursor: not-allowed;
+      > .ark-span-arrow {
+        border: none;
+      }
+    }
     &.ark-theme-button-active {
       background-color: $link-bg-active;
       color: $link-text-active;
@@ -156,9 +298,6 @@ button.ark-button {
       }
     }
     &.ark-size-big {
-      font-size: 24px;
-      height: $h + 16px;
-      padding: 0 32px;
       > .ark-span-arrow {
         top: calc(50% - 2.5px);
         border-top: 6px solid transparent;
@@ -167,9 +306,6 @@ button.ark-button {
       }
     }
     &.ark-size-small {
-      font-size: 8px;
-      height: $h - 8px;
-      padding: 0 16px;
       > .ark-span-arrow {
         top: calc(50% - 1.5px);
         border-top: 3px solid transparent;
@@ -183,6 +319,7 @@ button.ark-button {
     background-color: transparent;
     border: 1px solid transparent;
     color: $text-color-gr;
+    transition: color 250ms;
     &::before,
     &::after {
       content: "";
@@ -190,7 +327,8 @@ button.ark-button {
       width: 100%;
       height: 25%;
       border: 1px solid $text-color-gr;
-      transition: height 250ms;
+      transition-property: height, border-color;
+      transition-duration: 250ms;
     }
     &::before {
       top: 0;
@@ -202,43 +340,98 @@ button.ark-button {
       left: -1px;
       border-top: none;
     }
-    &.ark-size-big {
-      font-size: 24px;
-      height: $h + 16px;
-      padding: 0 32px;
-    }
     > .ark-loadingIndicator {
       border-color: $text-color-gr $text-color-gr $text-color-gr transparent;
     }
-    &.ark-size-small {
-      font-size: 8px;
-      height: $h - 8px;
-      padding: 0 16px;
+    &:hover {
+      color: #22bbff;
+      &::after,
+      &::before {
+        height: 50%;
+        border-color: #22bbff;
+      }
     }
-
-    &:hover::after,
-    &:hover::before {
-      height: 50%;
+    &[disabled] {
+      cursor: not-allowed;
+      &::before,
+      &::after {
+        height: 50%;
+      }
+      &:hover {
+        color: $text-color-gr;
+        &::after,
+        &::before {
+          border-color: $text-color-gr;
+        }
+      }
     }
   }
-  > .ark-loadingIndicator {
-    display: inline-block;
-    width: 14px;
-    height: 14px;
-    margin-right: 4px;
-    border-radius: 8px;
-    border-color: #fff #fff #fff transparent;
-    border-style: solid;
-    border-width: 2px;
-    animation: ark-spin 1s infinite linear;
+  &.ark-theme-danger {
+    border: 1px solid transparent;
+    background-image: linear-gradient(
+      135deg,
+      rgba(109, 27, 19, 0.91),
+      rgba(106, 19, 19)
+    );
+    &.ark-theme-button-active {
+      background-image: none;
+      background-color: #5b0d0d;
+    }
+    &[disabled] {
+      cursor: not-allowed;
+      > .ark-span-diabled {
+        position: absolute;
+        width: calc(100% + 2px);
+        height: calc(100% + 2px);
+        border-radius: $radius;
+        background-color: rgba(199, 199, 199, 0.7);
+      }
+    }
   }
 }
-@keyframes ark-spin {
+@keyframes show {
+  0%,
+  40% {
+    opacity: 0;
+  }
+  41%,
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes pulse {
+  0%,
+  40% {
+    background: #fdfdfd;
+  }
+  41%,
+  100% {
+    background: #dadada;
+  }
+}
+@keyframes ark-circle-spin {
   0% {
     transform: rotate(0deg);
   }
   100% {
     transform: rotate(360deg);
+  }
+}
+@keyframes ark-cube-angle {
+  0%,
+  10% {
+    opacity: 0;
+    transform: perspective(140px) rotateX(-180deg);
+  }
+  25%,
+  75% {
+    opacity: 1;
+    transform: perspective(140px) rotateX(0deg);
+  }
+  90%,
+  100% {
+    opacity: 0;
+    transform: perspective(140px) rotateX(180deg);
   }
 }
 </style>
