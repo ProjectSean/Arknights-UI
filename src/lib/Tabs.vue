d78cf8b874128f78d80775407a3e26555e4c275a
sean <seanrz0066@gmail.com>
2021-05-19 17:38:57 +0800

    查找子组件

---
+++ src/lib/Tabs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Tabs组件
+    <component :is="defaults[0]">
+    <component :is="defaults[1]">
+    </div>
+</template>
+
+<script>
+import Tab from "./Tab.vue";
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs子标签必须是Tab");
+      }
+    });
+    return { defaults };
+  },
+  components: {
+    Tab,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
