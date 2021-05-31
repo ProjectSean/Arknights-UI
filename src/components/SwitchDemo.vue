1fc700f192c56ee2abb3227d320869251525ffe0
sean <seanrz0066@gmail.com>
2021-05-31 15:02:18 +0800

    switch demo的基本页面

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -33,8 +33,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h1,
-.demo {
+// h1,
+// .demo {
+//   color: #fff;
+// }
+$border-color: #d9d9d9;
+h1 {
   color: #fff;
+}
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  color: #fff;
+  > h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    > pre {
+      line-height: 1.1;
+      font-family: Consolas, "Courier New", Courier, monospace;
+      margin: 0;
+      color: #fff;
+    }
+  }
 }
 </style>
