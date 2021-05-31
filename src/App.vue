44eec524a8fd7f69ffcf83586927f17f5674890f
sean <seanrz0066@gmail.com>
2021-05-31 18:49:59 +0800

    config配置更改

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,7 @@ import { router } from "./network/router";
 export default {
   name: "App",
   setup() {
-    // 获取页面宽度
     const bwidth = document.documentElement.clientWidth;
-    // 如果小于500则是移动端，值应该为500，则菜单默认不显示
     const menuVisible = ref(bwidth <= 500 ? false : true);
     provide("menuVisible", menuVisible);
     const homeMenuVisible = ref(bwidth <= 1080 ? false : true);
