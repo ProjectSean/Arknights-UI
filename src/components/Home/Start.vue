b7ed2577909a7503ebdc6ed626282d1d4b9b334c
sean <seanrz0066@gmail.com>
2021-06-02 13:02:11 +0800

    修改首页引用组件库方式

--- src/components/Home/Start.vue
+++ src/components/Home/Start.vue
@@ -3,7 +3,7 @@
     <h1>开始使用</h1>
     <p>请先<a href="#/home/training">安装</a>本组件库。</p>
     <p>然后在你的代码中写入下面的代码</p>
-    <pre><code>import {Button, Tabs, Switch, Dialog} from "ark-ui";</code></pre>
+    <pre><code>import {Button, Tabs, Switch, Dialog} from "arknights-ui";</code></pre>
     就可以使用我提供的组件了
     <h2>Vue 单文件组件</h2>
     <p>代码示例：</p>
@@ -13,7 +13,7 @@
     &lt;div&gt;&lt;Switch&gt;按钮&lt;/Switch&gt;&lt;/div&gt;
   &lt;/template&gt;
   &lt;script&gt;
-    import {Button, Tabs, Switch, Dialog} from "ark-ui";
+    import {Button, Tabs, Switch, Dialog} from "arknights-ui";
     export default {
       components: {Switch};
     }
--- src/components/Home/Training.vue
+++ src/components/Home/Training.vue
@@ -2,9 +2,9 @@
   <article class="markdown-body">
     <h1>安装</h1>
     <p>打开终端运行下列命令：</p>
-    <pre><code>npm install ark-ui</code></pre>
+    <pre><code>npm install arknights-ui</code></pre>
     <p>或</p>
-    <pre><code>yarn add ark-ui</code></pre>
+    <pre><code>yarn add arknights-ui</code></pre>
     <p>下一节：<a href="#/home/start">开始使用</a></p>
   </article>
 </template>
